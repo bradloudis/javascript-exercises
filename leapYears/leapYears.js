ced90961c70cd39b7a83277508f14cb824e09a3b
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-01 12:37:31 -0500

    pass all leapYears tests

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  // check for century years
+  if (year % 100 === 0) {
+    return year % 400 === 0 ? true : false;
+  }
 
-}
+  // check non-century years
+  if (year % 4 === 0) {
+    return true;
+  }
 
-module.exports = leapYears
+  // default return bool set to false
+  return false;
+};
+
+module.exports = leapYears;
