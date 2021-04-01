892aa78ae433b8cf8c9abb35f150239c1abaee7f
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-01 12:49:23 -0500

    pass all tempConversion tests

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,12 +1,16 @@
-const ftoc = function() {
+const ftoc = function (num) {
+  const tempInC = (num - 32) * (5 / 9);
+  const rounded = parseFloat(tempInC.toFixed(1));
+  return rounded;
+};
 
-}
-
-const ctof = function() {
-
-}
+const ctof = function (num) {
+  const tempInF = num * (9 / 5) + 32;
+  const rounded = parseFloat(tempInF.toFixed(1));
+  return rounded;
+};
 
 module.exports = {
   ftoc,
-  ctof
-}
+  ctof,
+};
