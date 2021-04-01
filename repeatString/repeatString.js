059edb62d22a98ae0a2d116cc8db14415d74660e
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-01 11:47:57 -0500

    pass all tests for repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let returnString = '';
 
-}
+  // return 'ERROR' if num is negative
+  if (num < 0) {
+    return 'ERROR';
+  }
 
-module.exports = repeatString
+  for (let i = 1; i <= num; i++) {
+    returnString += string;
+  }
+  return returnString;
+};
+
+module.exports = repeatString;
