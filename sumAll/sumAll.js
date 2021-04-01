a3472bcaa1882860ee67064ecd253e3d5e719447
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-01 12:22:49 -0500

    pass all tests for sumAll

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,32 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  let sum = 0;
+  let start;
+  let end;
 
-}
+  // return ERROR if a or b is negative
+  if (a < 0 || b < 0) {
+    return 'ERROR';
+  }
 
-module.exports = sumAll
+  // return ERROR if a or b are non-numbers
+  if (typeof a !== 'number' || typeof b !== 'number') {
+    return 'ERROR';
+  }
+
+  // set start and end pts for sum loop
+  if (a > b) {
+    start = b;
+    end = a;
+  } else {
+    start = a;
+    end = b;
+  }
+
+  for (let i = start; i <= end; i++) {
+    sum += i;
+  }
+
+  return sum;
+};
+
+module.exports = sumAll;
