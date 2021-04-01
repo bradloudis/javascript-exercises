ed01477a15a48c1002178511332e970eb038dd75
Brad Loudis <Bradley.Loudis@gmail.com>
2021-04-01 12:13:37 -0500

    pass all tests for removeFromArray

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...items) {
+  for (let item of items) {
+    const index = array.indexOf(item);
+    if (index === -1) {
+      continue;
+    } else {
+      array.splice(index, 1);
+    }
+  }
 
-}
+  return array;
+};
 
-module.exports = removeFromArray
+module.exports = removeFromArray;
